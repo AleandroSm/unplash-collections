08272c001da9ed3e88301ff9bec5c401ac395809
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-25 17:06:10 -0600

    Refactor useCollection hook to include modal functionality and state management

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -4,32 +4,40 @@ import { addPhotoToSelectedCollection, removePhotoFromSelectedCollection, select
 import {startAddCollection, startAddPhotoToCollection, startDeletePhotoFromCollection} from "../store/collections/thunks"
 import {toast} from "react-hot-toast"
 import { useNavigate } from "react-router-dom"
+import { useModal } from "./useModal"
+import { useState } from "react"
 
 export const useCollection = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const {handleCloseModal} = useModal()
     const {collections} = useSelector(state => state.collections)
     const {selectedPhoto} = useSelector(state => state.photos)
     const notify = (message) => toast.success(message)
     const notifyError = (message) => toast.error(message)
-    let photoInCollections = []
-    let photoNotInCollections = []
-
-    photoInCollections = collections.filter(collection => collection.photosUrls.some(url => url.slice(0,url.indexOf('?')) === selectedPhoto.urls.regular.slice(0,selectedPhoto.urls.regular.indexOf('?'))))
-    photoNotInCollections = collections.filter(collection => collection.photosUrls.every(url => url.slice(0,url.indexOf('?')) !== selectedPhoto.urls.regular.slice(0,selectedPhoto.urls.regular.indexOf('?'))))
+    const photoInCollections = collections.filter(collection => collection.photosUrls.some(url => url.slice(0,url.indexOf('?')) === selectedPhoto?.urls.regular.slice(0,selectedPhoto?.urls.regular.indexOf('?'))))
+    const photoNotInCollections = collections.filter(collection => collection.photosUrls.every(url => url.slice(0,url.indexOf('?')) !== selectedPhoto?.urls.regular.slice(0,selectedPhoto?.urls.regular.indexOf('?'))))
+    const [collectionsModal, setCollectionsModal] = useState(photoNotInCollections)
 
     const existsCollection = (name) => collections.some(collection => collection.name.toLowerCase() === name.toLowerCase())
 
     const addPhotoToCollection = (name) => {
         dispatch(selectCollectionByName(name))
-        dispatch(addPhotoToSelectedCollection(selectedPhoto.urls.regular))
+        dispatch(addPhotoToSelectedCollection(selectedPhoto?.urls.regular))
         dispatch(startAddPhotoToCollection())
         notify("Photo added to collection")
+        handleCloseModal()
+        setCollectionsModal(photoNotInCollections)
+    }
+
+    const searchCollectionsInModal = (name = "") => {
+        if(name.length <= 3) setCollectionsModal(photoNotInCollections)
+        else setCollectionsModal(photoNotInCollections.filter(collection => collection.name.toLowerCase().includes(name.toLowerCase())))
     }
 
     const removePhotoFromCollection = (name) => {
         dispatch(selectCollectionByName(name))
-        dispatch(removePhotoFromSelectedCollection(selectedPhoto.urls.regular.slice(0,selectedPhoto.urls.regular.indexOf('?'))))
+        dispatch(removePhotoFromSelectedCollection(selectedPhoto?.urls.regular.slice(0,selectedPhoto.urls.regular.indexOf('?'))))
         dispatch(startDeletePhotoFromCollection())
         notify("Photo removed from collection")
     }
@@ -38,6 +46,7 @@ export const useCollection = () => {
         if(!existsCollection(name)){
             dispatch(startAddCollection(name))
             notify("Collection created")
+            handleCloseModal()
         }else notifyError("Collection already exists")
     }
 
@@ -54,7 +63,9 @@ export const useCollection = () => {
         createCollecttion,
         removePhotoFromCollection,
         selectedPhoto,
-        selectedCollection
+        selectedCollection,
+        collectionsModal,
+        searchCollectionsInModal
     }
 
 }
