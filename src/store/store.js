858333cc191dca679b661a68c43a40acc1111899
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-25 17:05:14 -0600

    Add modalSlice to manage modal state

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import photosSlice from './photos/photosSlice'
 import collectionsSlice from './collections/collectionsSlice'
+import modalSlice from './modal/modalSlice'
 
 export const store = configureStore({
   reducer: {
     photos: photosSlice,
-    collections: collectionsSlice
+    collections: collectionsSlice,
+    modal: modalSlice
   },
 })
