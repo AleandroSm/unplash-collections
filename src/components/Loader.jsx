d4d0b214e84bb5ce7a6d95dce948a12d8a8c3d30
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-26 13:53:44 -0600

    Add Loader component for displaying a loading spinner

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+
+export const Loader = () => {
+
+    return (
+        <div 
+        className="h-20 w-20 border-4 border-dashed rounded-full animate-spin border-black mr-auto ml-auto"
+        >
+        
+        </div>
+    )
+}
