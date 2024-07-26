bc0eef83634ca6258657cace0cfccb8bf32c2e06
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-26 13:53:08 -0600

    Update CollectionItem component to use react-hot-toast for custom notifications

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,8 +1,33 @@
+import { Toaster, toast } from "react-hot-toast"
+import CustomToast from "./Toast"
+import { useSelector } from "react-redux"
 
-export const CollectionItem = ({collection, text,icon, onClick}) => {
+export const CollectionItem = ({collection, text,icon, addItem, removeItem}) => {
+
+    const {selectedPhoto}  = useSelector(state => state.photos)
+
+    const customNotify = () => {
+        toast.custom((t) => (
+            <CustomToast 
+                t={t}
+                name={"photo added in"}
+                message={`${collection.name} collection`}
+                imgSrc={selectedPhoto.urls.regular}
+            />
+        ))
+    }
+
+    const updateItem = (name) => {
+        if(addItem) {
+            addItem(name)
+            customNotify()
+        }
+        if(removeItem) removeItem(name)
+    }
+
     return (
         <div className='flex items-center gap-4 hover:bg-gray-300 rounded-xl relative cursor-pointer
-        ' onClick={() => onClick(collection.name)}>
+        ' onClick={() => updateItem(collection.name)}>
             <img src={collection.photosUrls} alt="" className='w-24 h-24 rounded-lg py-3 pl-2 ' />
             <div>
                 <p className='font-bold'>{collection.name}</p>
@@ -13,6 +38,7 @@ export const CollectionItem = ({collection, text,icon, onClick}) => {
                 <img src={icon} alt="icon" className=""/>
                 <span className="text-sm font-semibold">{text}</span>
             </div>
+            < Toaster />
         </div>
     )
 }
