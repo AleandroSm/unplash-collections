d2ca03742257b9725e3efcee08f0cc85e8a1363d
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-26 13:52:21 -0600

    Refactor PhotosView component to improve performance and readability

--- src/views/PhotoView.jsx
+++ src/views/PhotoView.jsx
@@ -1,25 +1,19 @@
-import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { usePhoto } from "../hooks/usePhoto"
 
-export const PhotosView = () => {
-    const {query} = useParams()
-    const {handleSelectPhoto} = usePhoto(query)
-    const {photos} = useSelector(state => state.photos)
-
+export const PhotosView = ({photos, navigateToSelectecPhoto}) => {
+    
     return (
         <div className="columns-2 md:columns-4 gap-4 md:px-20 px-4">
             {
-                photos.length > 0 && photos.map((photo) => (
+                photos.length > 0  ?photos.map((photo) => (
                     <img 
                     src={photo.urls.regular} 
                     loading="lazy"
                     alt={photo.alt_description} key={photo.id} 
                     className=" rounded-lg cursor-pointer max-w-full mt-4 block"
-                    onClick={() => handleSelectPhoto(photo)}
+                    onClick={() => navigateToSelectecPhoto(photo)}
                     />
-                    
                 ))
+                : <div className="text-red-500">No Photos</div>
             }
         </div>
     )
