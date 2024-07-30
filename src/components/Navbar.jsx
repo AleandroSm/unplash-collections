026b62d9a5366e6592a14b8b254e21b018740a8e
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-30 12:39:58 -0600

    refactor: Update Navbar component to use NavLink for active navigation
    This commit updates the Navbar component to use the NavLink component from react-router-dom for active navigation. It adds the necessary logic to determine the active route and applies the appropriate styling to the corresponding navigation item. This improves the user experience by providing visual feedback for the current page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,38 @@
-import logo from '../assets/Logo.svg'
-import { Link } from 'react-router-dom'
+import logo from '../assets/Logo.svg';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const NavBar = () => {
+    const location = useLocation();
+
+    const isCollectionsActive = () => location.pathname.startsWith('/collections');
+
+    const isHomeActive = () => !location.pathname.includes("collections");
+
     return (
         <>
-        <header className='md:px-12 py-4 w-full'>
-            <nav className='flex justify-between w-full'>
-                <img src={logo} alt="" />
-                <ul className='font-semibold text-sm'>
-                    <li className='inline-block py-2 px-4 mr-2 md:mr-4 text-black bg-slate-400'>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className='inline-block text-slate-600 mr-2'>
-                        < Link to="/collections" >Collections</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        <hr />
+            <header className='md:px-12 py-4 w-full'>
+                <nav className='flex justify-between w-full'>
+                    <img src={logo} alt="Logo" />
+                    <ul className='font-semibold text-sm flex'>
+                        <li
+                            className={`inline-block py-2 px-4 mr-2 md:mr-4 text-black ${isHomeActive() ? 'bg-slate-400' : ''}`}
+                        >
+                            <NavLink to='/'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li
+                            className={`inline-block py-2 px-4 mr-2 text-black ${isCollectionsActive() ? 'bg-slate-400' : ''}`}
+                        >
+                            <NavLink to='/collections'>
+                                Collections
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <hr />
         </>
-    )
-}
+    );
+};
+ 
