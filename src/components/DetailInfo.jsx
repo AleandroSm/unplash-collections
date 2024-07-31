bfc4b8f96ee253aa5627ceca5dd139eb92153d87
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-30 18:22:33 -0600

    The handleDownloadPhoto function is implemented to fetch the photo, create a blob object, and generate a download link

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -2,9 +2,22 @@ import plusIcon from "../assets/Plus.svg"
 import downloadIcon from "../assets/downArrow.svg"
 import { useModal } from "../hooks/useModal"
 
-export const DetailInfo = () => {
+export const DetailInfo = ({photo}) => {
 
     const {handleOpenModal} = useModal()
+    const handleDownloadPhoto = async () => {
+        const url = photo.urls.raw.split('?')[0]
+        const name = photo.slug
+        const data = await fetch(url)
+        const blob = await data.blob()
+        const urlBlob = URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = urlBlob
+        a.download = name
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+    }
 
     return (
         <div className="mt-6 font-bold">
@@ -17,10 +30,12 @@ export const DetailInfo = () => {
             </button>
             <button
                 className="bg-gray-200 px-8 py-3 mt-4 md:mt-0 text-sm relative "
+                onClick={handleDownloadPhoto}
             >
                 <img src={downloadIcon} alt="" className="absolute left-3 top-[35%]" />
                 Download
             </button>
+            
         </div>
     )
 }
