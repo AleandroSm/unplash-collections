c0922e747cf9dd793d3b39c92976cf46422456e3
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-30 19:20:52 -0600

    feat: Add lazy loading for images in CollectionItem component
    This commit adds lazy loading functionality for images in the CollectionItem component. By using the `react-lazyload` library, images will be loaded only when they are visible in the viewport, improving the page load performance. This enhancement provides a better user experience by reducing the initial load time of the page.

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,26 +1,16 @@
 import { Toaster, toast } from "react-hot-toast"
-import CustomToast from "./Toast"
 import { useSelector } from "react-redux"
+import { useToast } from "../hooks/useToast"
 
 export const CollectionItem = ({collection, text,icon, addItem, removeItem}) => {
 
     const {selectedPhoto}  = useSelector(state => state.photos)
-
-    const customNotify = () => {
-        toast.custom((t) => (
-            <CustomToast 
-                t={t}
-                name={"photo added in"}
-                message={`${collection.name} collection`}
-                imgSrc={selectedPhoto.urls.regular}
-            />
-        ))
-    }
+    const {customNotify} = useToast()
 
     const updateItem = (name) => {
         if(addItem) {
             addItem(name)
-            customNotify()
+            customNotify({name: "photo added in ", message: `${collection.name} collection`, image: selectedPhoto.urls.regular})
         }
         if(removeItem) removeItem(name)
     }
@@ -28,10 +18,10 @@ export const CollectionItem = ({collection, text,icon, addItem, removeItem}) => 
     return (
         <div className='flex items-center gap-4 hover:bg-gray-300 rounded-xl relative cursor-pointer
         ' onClick={() => updateItem(collection.name)}>
-            <img src={collection.photosUrls} alt="" className='w-24 h-24 rounded-lg py-3 pl-2 ' />
+            <img src={collection?.photos[0]?.urlPhoto} alt={collection?.photos?.alt} className='w-24 h-24 rounded-lg py-3 pl-2 ' />
             <div>
                 <p className='font-bold'>{collection.name}</p>
-                <span className="text-sm">{`${collection.photosUrls.length} Photos`}</span>
+                <span className="text-sm">{`${collection?.photos?.length} Photos`}</span>
             </div>
         
             <div className=" ml-[15%] w-[50%] gap-2 flex justify-end">
@@ -44,8 +34,7 @@ export const CollectionItem = ({collection, text,icon, addItem, removeItem}) => 
                         duration: 1000,
                     }
 
-                }}
-            
+                }}            
             />
         </div>
     )
