b06b9e56b7856e393dc7f9747362cde01bd011e7
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-07-30 19:21:51 -0600

    feat: Improve rendering and alt text for CollectionPage images
    This commit enhances the CollectionDetailPage component by improving the rendering of images and alt text. It replaces the deprecated `selectedCollection.photosUrls` with `selectedCollection.photos[0]?.urlPhoto` for the image source and `selectedCollection.photos[0]?.alt` for the alt text. Additionally, it adds the `mr-4` class to the image for proper spacing.These changes ensure that the correct image is displayed and provide meaningful alt text for accessibility purposes.

--- src/pages/CollectionDetailPage.jsx
+++ src/pages/CollectionDetailPage.jsx
@@ -1,25 +1,31 @@
+import { useNavigate } from "react-router-dom"
 import { NavBar } from "../components/Navbar"
 import { useSelector } from "react-redux"
 
 export const CollectionDetailPage = () => {
-
+  const navigate = useNavigate()
   const {selectedCollection} = useSelector(state => state.collections)
 
+  const navigateToDetailPhoto = (photo) => {
+    navigate(`/detail/${photo.idPhoto}`)
+  }
+  
   return (
     <>
 
       < NavBar /> 
 
       <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#F2C593] to-[#8A3282] text-5xl mt-6 text-center">{selectedCollection.name}</h1> 
-      <p className="mt-3 text-center">{`${selectedCollection.photosUrls.length} Photos`}</p>
+      <p className="mt-3 text-center">{`${selectedCollection?.photos?.length} Photos`}</p>
 
       <div className="columns-2 px-4 gap-4 md:columns-4 md:px-20 mt-12">
             {
-                selectedCollection.photosUrls.map((photo, index) => (
+                selectedCollection.photos.map((photo, index) => (
                     <img 
-                    src={photo} 
-                    alt="photo of unplash" key={index} 
+                    src={photo.urlPhoto} 
+                    alt={photo.alt} key={index} 
                     className="rounded-xl cursor-pointer max-w-full mt-4"
+                    onClick={ () => navigateToDetailPhoto(photo)}
                     />
                     
                 ))
