38421c6326b7e741f2b4778d21e8380a3cf0c129
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-08-01 15:26:08 -0600

    Update unsplashApi access key handling.

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
-import { createApi } from "unsplash-js";
+import { createApi } from "unsplash-js"
+
+const accessKey = import.meta.env.VITE_ACCESS_KEY
 
 
-export const unsplashApi = createApi({accessKey: "OklZRVxmJrLIHP9lttP5KPBINathY7Wblwke-HldDxM"})
+export const unsplashApi = createApi({accessKey: accessKey})
 
 
    
