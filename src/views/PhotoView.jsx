8a50fcd134c3d752785fc6d55a56aed53a7f5c0a
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-08-02 12:49:59 -0600

    refactor: Update PhotoView component to use BlurHashLazyPhoto for lazy loading and blurhash rendering

--- src/views/PhotoView.jsx
+++ src/views/PhotoView.jsx
@@ -1,31 +1,18 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
-
+import { BlurHashLazyPhoto } from '../components/BlurHashLazyPhoto';
 
 export const PhotosView = ({photos, navigateToSelectecPhoto}) => {
     
     return (
-        <div className="columns-2 md:columns-4 gap-4 md:px-20 px-4">
+        <div className="columns-2 md:columns-4 gap-4 md:px-20 px-4 ">
             {
-                photos.length > 0  ?photos.map((photo) => (
-                    <div className={`h-[400px]`} key={photo.id}>
-                        < LazyLoadImage 
-                            src={photo?.urls?.regular}
-                            alt={photo?.alt_description}
-                            className='rounded-lg cursor-pointer max-w-full mt-4 object-cover h-full'
-                            width='100%'
-                            onClick={() => navigateToSelectecPhoto(photo)}
-                            effect='blur'
-                            wrapperProps={{style: {transitionDelay: '200ms'}}}
+                photos.length > 0 ? photos.map((photo) => (
+                    <div className={`h-[400px] mt-4`} key={photo.id} onClick={() => navigateToSelectecPhoto(photo)}>
+                        
+                        < BlurHashLazyPhoto 
+                            photo={photo}
+                            className="rounded-lg cursor-pointer max-w-full object-cover h-full"
                         />
-                        {/* <img 
-                        src={photo?.urls?.regular} 
-                        loading="lazy"
-                        alt={photo?.alt_description} key={photo.id} 
-                        className={`rounded-lg cursor-pointer max-w-full mt-4 object-cover h-full`}
-                        onClick={() => navigateToSelectecPhoto(photo)}
-                        /> */}
-
+                        
                     </div>
                 ))
                 : <div className="text-red-500">No Photos</div>
