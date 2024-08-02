f62a1bb7fadaf8d1891c667037ef7325e5dadd97
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-08-02 12:48:49 -0600

    feat: Add trash icon and removeCollection functionality to CollectionDetailPage

--- src/pages/CollectionDetailPage.jsx
+++ src/pages/CollectionDetailPage.jsx
@@ -1,23 +1,33 @@
 import { useNavigate } from "react-router-dom"
 import { NavBar } from "../components/Navbar"
 import { useSelector } from "react-redux"
+import trashIcon from '../assets/trash.svg'
+import { startDeleteCollection } from "../store/collections/thunks"
+import { useDispatch } from "react-redux"
 
 export const CollectionDetailPage = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const {selectedCollection} = useSelector(state => state.collections)
-
   const navigateToDetailPhoto = (photo) => {
     navigate(`/detail/${photo.idPhoto}`)
   }
+
+   const removeCollection = () => {
+    dispatch(startDeleteCollection())
+    navigate("/collections")
+   } 
   
   return (
     <>
-
       < NavBar /> 
-
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#F2C593] to-[#8A3282] text-5xl mt-6 text-center">{selectedCollection.name}</h1> 
-      <p className="mt-3 text-center">{`${selectedCollection?.photos?.length} Photos`}</p>
-
+      <div className="grid place-content-center">
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#F2C593] to-[#8A3282] text-5xl mt-6 text-center">{selectedCollection.name}</h1> 
+        <div className="text-center mt-2 md:hidden" onClick={removeCollection}>
+        <img src={trashIcon} alt="trash icon" className="w-6 h-6 inline-block"/>
+        </div>
+        <p className="mt-3 text-center">{`${selectedCollection?.photos?.length} Photos`}</p>
+      </div>
       <div className="columns-2 px-4 gap-4 md:columns-4 md:px-20 mt-12">
             {
                 selectedCollection.photos.map((photo, index) => (
