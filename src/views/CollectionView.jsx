975921f36f04fb5cf8839338b64be2ebd97cfb49
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-08-02 12:48:23 -0600

    feat: Improve CollectionView with CollectionOption component and mouse hover functionality

--- src/views/CollectionView.jsx
+++ src/views/CollectionView.jsx
@@ -1,6 +1,26 @@
 import addIconCollection from '../assets/Plus.svg'
+import { CollectionOption } from '../components/CollectionOption'
 
-export const CollectionView = ({ collections, selectedCollection, openModal }) => {
+export const CollectionView = ({ collections, selectedCollection,removeCollection,openModal }) => {
+
+    const handleMouseEnter = (e) => {
+        
+        const target = document.getElementById(e.target.id)
+        const options = document.getElementsByClassName('options')
+        for(const option of options){
+            if(option.id === target.id){
+                option.classList.remove('invisible')
+            }
+        }
+    }
+
+    const handleMouseLeave = (e) => {
+        const options = document.getElementsByClassName('options')
+        for(const option of options){
+            option.classList.add('invisible')
+        }        
+    }
+
     return (
         <>
         <img 
@@ -11,13 +31,17 @@ export const CollectionView = ({ collections, selectedCollection, openModal }) =
         <div className="mt-12 grid grid-cols-auto-fit md:px-16 px-0 gap-4">
             {
                 collections.map(collection => (
-                    <div key={collection.id} className="text-center">
+                    <div key={collection.id} className="text-center relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+                       
+                       <CollectionOption id={collection.id} removeCollection={() => removeCollection(collection.name)}/>
                         <img
                             src={collection.photos[0]?.urlPhoto}
                             alt={collection.photos[0]?.alt}
                             className="h-60 w-1/2 md:w-full rounded-sm object-cover cursor-pointer inline-block mr-4"
                             onClick={() => selectedCollection(collection.name)}
+                            id={collection.id}
                             />
+
                         <h3 className="mt-3 font-bold">{collection.name}</h3>
                         <small className="text-gray-400">{`${collection?.photos?.length} Photos`}</small>
                     </div>
