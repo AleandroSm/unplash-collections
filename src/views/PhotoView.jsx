c8e0523e669d48b8bc14fa627726c09cd5431662
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-08-01 21:48:34 -0600

    feat: Implement lazy loading for images in PhotoView
    This commit adds lazy loading functionality for images in the PhotoView component. By importing the `react-lazy-load-image-component` library and using it to render the images, the page load performance is improved. This enhancement provides a better user experience by reducing the initial load time of the page.

--- src/views/PhotoView.jsx
+++ src/views/PhotoView.jsx
@@ -1,3 +1,6 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 
 export const PhotosView = ({photos, navigateToSelectecPhoto}) => {
     
@@ -5,13 +8,25 @@ export const PhotosView = ({photos, navigateToSelectecPhoto}) => {
         <div className="columns-2 md:columns-4 gap-4 md:px-20 px-4">
             {
                 photos.length > 0  ?photos.map((photo) => (
-                    <img 
-                    src={photo.urls.regular} 
-                    loading="lazy"
-                    alt={photo.alt_description} key={photo.id} 
-                    className=" rounded-lg cursor-pointer max-w-full mt-4 block"
-                    onClick={() => navigateToSelectecPhoto(photo)}
-                    />
+                    <div className={`h-[400px]`} key={photo.id}>
+                        < LazyLoadImage 
+                            src={photo?.urls?.regular}
+                            alt={photo?.alt_description}
+                            className='rounded-lg cursor-pointer max-w-full mt-4 object-cover h-full'
+                            width='100%'
+                            onClick={() => navigateToSelectecPhoto(photo)}
+                            effect='blur'
+                            wrapperProps={{style: {transitionDelay: '200ms'}}}
+                        />
+                        {/* <img 
+                        src={photo?.urls?.regular} 
+                        loading="lazy"
+                        alt={photo?.alt_description} key={photo.id} 
+                        className={`rounded-lg cursor-pointer max-w-full mt-4 object-cover h-full`}
+                        onClick={() => navigateToSelectecPhoto(photo)}
+                        /> */}
+
+                    </div>
                 ))
                 : <div className="text-red-500">No Photos</div>
             }
